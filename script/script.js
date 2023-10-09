273f668ed129737598e4278cd7ad1764fbf75c95
“Ygor <igorgarofalo@gmail.com>
2023-10-09 13:40:08 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+}
